ea4df3c92640d6eb81f8506fbd59ce7ceece6af3
Daury P. <dauryjey@gmail.com>
2024-07-11 09:37:34 -0400

    chore: Add initial server setup with Express and TypeScript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Application, Response } from "express"
+import cors from "cors"
+
+const app: Application = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (_, res: Response) => {
+	res.status(200).send("It's working")
+})
+
+app.listen(PORT, () => {
+	console.log("Server listening on PORT:", PORT)
+})
+
+export default app
