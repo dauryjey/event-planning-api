f62c4c6dc622721d99b9e53cbf58cf6352f26d38
Daury P. <dauryjey@gmail.com>
2024-07-12 22:37:41 -0400

    chore: Add express base

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_, res: Response) => {
 	res.status(200).send("It's working")
